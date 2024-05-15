6b77f625a3fc6dac2ca14a972cba0a65154379b9
idiomas <idiomas@SJ106PC3.fup.edu>
2024-05-15 11:45:23 -0500

    Clase Parking Metodo Entrada

--- src/Parking/Parking.java
+++ src/Parking/Parking.java
@@ -1,7 +1,45 @@
 
 package Parking;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 
 public class Parking {
     
+    private ArrayList<String> matriculas;
+    private String nombre;
+
+    public Parking(String nombre, int posicion) {
+        this.nombre = nombre;
+        matriculas = new ArrayList<>(Collections.nCopies(posicion, null));
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public void entrada(String matricula, int posicion) throws ParkingException{
+        if(posicion>=matriculas.size() || posicion <0)
+            throw new ParkingException("Posicion Inexistente",matricula);
+        if(matricula==null || matricula.length()<4)
+            throw new ParkingException("Matricula Incorrecta", matricula);
+        if(matriculas.get(posicion)!=null)
+            throw new ParkingException("Posición Ocupada", matricula);
+        if(matriculas.contains(matricula))
+            throw new ParkingException("Matricula Repetida", matricula);
+        matriculas.set(posicion, matricula);
+    
+    }
+
+    private static class ParkingException extends Exception {
+
+        public ParkingException(String posicion_Inexistente, String matricula) {
+        }
+    }
+    
+  
+    
+   
+            
 }
